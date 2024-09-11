647d9d91ab2d04f75579dc6e18b6d5add52af631
zhy21345 <46223794+zhy21345@users.noreply.github.com>
2024-09-11 15:30:20 +0800

    test commit on edit from vsc web

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,9 @@ import IntroBar from "./components/intro-bar.jsx"
 export default function Home() {
   return (
     <main>
+      <p>
+        test new here.
+      </p>
       <IntroBar/>
     </main>
   );
