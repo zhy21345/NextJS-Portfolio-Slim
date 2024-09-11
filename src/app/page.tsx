0f2e2f1f3996f6a65ab5942a373770be5faed644
zhy21345 <46223794+zhy21345@users.noreply.github.com>
2024-09-11 15:33:32 +0800

    clear test for web dev

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,6 @@ import IntroBar from "./components/intro-bar.jsx"
 export default function Home() {
   return (
     <main>
-      <p>
-        test new here.
-      </p>
       <IntroBar/>
     </main>
   );
